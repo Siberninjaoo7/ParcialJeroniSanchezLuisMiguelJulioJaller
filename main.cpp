424fc31033c23eefcab1cc6aa443b4ef8dc47b1b
jero4204 <104170500+jero4204@users.noreply.github.com>
2023-09-07 20:36:18 -0500

    Se arreglan los errores
    Se arreglan los errores que se tenian en los tres metodos y se deja todo funcionando, además se agrega un mensaje inicial para indicar con que teclas se seleccionan los metodos

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
     #include <algorithm>
     #include <cmath>
     #include <cmath>
+    #include <ostream>
     using namespace std;
     #define BLINKING_RATE     100ms
 
@@ -21,25 +22,29 @@
         int xFuncion = -1, yFuncion = -1;
         //cout<<"hola"<<endl;
         while (true) {
-
-            for(int i =0; i<3; i++){
-                arregloCol[i] =0;
-                for(int j = 0; j<4;j++){
-                    if(!arregloFil[j]){
-                        xFuncion = i;
-                        yFuncion = j;
-                        while(!arregloFil[j]);
-                        break;
+            bool validadorPrincipal = true;
+            cout<<"Selecione la funcion deseada con 1, 2 o 3"<<endl;
+            while(validadorPrincipal){
+                for(int i =0; i<3; i++){
+                    arregloCol[i] =0;
+                    for(int j = 0; j<4;j++){
+                        if(!arregloFil[j]){
+                            xFuncion = i;
+                            yFuncion = j;
+                            validadorPrincipal=false;
+                            while(!arregloFil[j]);
+                            break;
+                        }
                     }
+                    arregloCol[i] =1;
                 }
-                arregloCol[i] =1;
             }
             if(xFuncion == 0 && yFuncion == 0){
                 cout<<"Primer funcion"<<endl;
                 double x1 =0,y1=0,x2=0,y2=0;
                 int counter =0;
 
-                cout<<"Ingrese la coordenada x del primer punto "<<endl;
+                cout<<"Ingrese la coordenada X del primer punto "<<endl;
                 bool validador= true;
                 while(validador){
                     for(int i =0; i<3; i++){
@@ -62,8 +67,8 @@
                 }
                 counter=0;
                 validador = true;
-                cout<<"X1 es igual a "<<x1<<endl;
-                cout<<"ingrese la coordenada Y del primer punto "<<endl;
+                cout<<"La coordenada X1 ingresada es "<<x1<<endl;
+                cout<<"Ingrese la coordenada Y del primer punto "<<endl;
                 while(validador){
                     for(int i =0; i<3; i++){
                         arregloCol[i] =0;
@@ -85,7 +90,7 @@
                 }
                 validador=true;
                 counter = 0;
-                cout<<"La coordenada de Y1 es"<<y1<<endl;
+                cout<<"La coordenada de Y1 ingresada es "<<y1<<endl;
                 cout<<"Ingrese la coordenada X del segundo punto "<<endl;
                 while(validador){
                     for(int i =0; i<3; i++){
@@ -108,7 +113,7 @@
                 }
                 validador = true;
                 counter =0;
-                cout<<"El valor de X2 es"<<x2<<endl;
+                cout<<"La coordenada de X2 ingresada es "<<x2<<endl;
                 cout<<"Ingrese la coordenada Y del segundo punto "<<endl;
                 while(validador){
                     for(int i =0; i<3; i++){
@@ -129,16 +134,16 @@
                         arregloCol[i] =1;
                     }
                 }
-                cout<<"El valor del Y2 es "<<y2<<endl;
+                cout<<"La coordenada de Y2 ingresada es "<<y2<<endl;
                 if(x1 ==x2 && y1 ==y2){
-                    cout<<"los puntos son iguales la pendiente es indefinida"<<endl;
+                    cout<<"Los puntos son iguales la pendiente es indefinida"<<endl;
                 }else if(x2-x1 == 0){
-                    cout<<"la pendiente e indefinida por division entre cero"<<endl;
+                    cout<<"La pendiente e indefinida por division entre cero"<<endl;
                 }else{
                     double pendiente = (y2-y1)/(x2-x1);
                     double interceptoY = y1-pendiente*x1;
                     cout<<"La pendiente es "<<pendiente<<endl;
-                    cout<<"El intercepto con el eje Y es"<<interceptoY<<endl;
+                    cout<<"El intercepto con el eje Y es "<<interceptoY<<endl;
                 }
                 xFuncion = -1;
                 yFuncion = -1;
@@ -157,7 +162,6 @@
                         for(int j = 0; j<4;j++){
                             if(!arregloFil[j]){
                                 n = n + teclado[j][i]*(pow(10,counter));
-                                cout<<temp<<endl;
                                 counter++;
                                 while(!arregloFil[j]){
 
@@ -168,7 +172,6 @@
                                 }
                             }
                         }
-                        
                         arregloCol[i] =1;
                     }
                 }
@@ -176,27 +179,28 @@
                 validador=true;
                 counter=0;
                 for(int u = 0; u<n;u++){
-                    cout<<"Ingrese la temperatura"<<endl;
+                    temp=0;
+                    cout<<"Ingrese la temperatura "<<u<<" :"<<endl;
                     while(validador){
                         for(int i =0; i<3; i++){
                             arregloCol[i] =0;
                             for(int j = 0; j<4;j++){
                                 if(!arregloFil[j]){
                                     temp += teclado[j][i]*(pow(10,counter));
-                                counter++;
-                                while(!arregloFil[j]){
+                                    counter++;
+                                    while(!arregloFil[j]){
 
-                                }
-                                if(i == 2 && j == 3){
-                                    validador = false;
-                                    break;  
-                                }
+                                    }
+                                    if(i == 2 && j == 3){
+                                        validador = false;
+                                        break;  
+                                    }
                                 }
                             }
                             arregloCol[i] =1;
                         }
                     }
-                    cout<<"La temp es "<<temp<<endl;
+                    cout<<"La temperatura ingresada es "<<temp<<endl;
                     validador=true;
                     counter=0;
                     suma += temp;
@@ -209,63 +213,112 @@
                 xFuncion = -1;
                 yFuncion = -1;
             }else if (xFuncion == 2 && yFuncion == 0){
-                cout<<"tercera funcion"<<endl;
-                int validador =1;
+                cout<<"Tercera funcion"<<endl;
+                int validador = 1;
                 while (validador == 1) {
-                    string NumeroRGB;
-                    cout<<"Ingrese el codigo hexaceimal deseado: "<<endl;
                     int frecuenciaR;
                     int frecuenciaG;
                     int frecuenciaB;
-                    int count = 0;
-                    int countR = 0;
-                    int countG = 0;
-                    int countB = 0;
+                    int counter = 0;
                     int R = 0;
                     int G = 0;
                     int B = 0;
-                    for(int i =0; i<3; i++){
-                        arregloCol[i] =0;
-                        for(int j = 0; j<4;j++){
-                
-                            if(!arregloFil[j]){
-                                if(count == 0){
-                                    R = R+teclado[i][j]*10^countR;
-                                    countR ++;
-                                    while(!arregloFil[j]);
-                                    if(countR == 3){
-                                        count++;
+                    bool validadorRGB = true;
+                    cout<<"Ingrese el valor (entre 0 y 255) para el rojo"<<endl;
+                    while(validadorRGB){
+                        for(int i =0; i<3; i++){
+                            arregloCol[i] =0;
+                            for(int j = 0; j<4;j++){
+                                if(!arregloFil[j]){
+                                    R += teclado[j][i]*(pow(10,counter));
+                                    counter++;
+                                    while(!arregloFil[j]){
+
                                     }
-                                }else if(count == 1){
-                                    G = G+teclado[i][j]*10^countG;
-                                    countG ++;
-                                    while(!arregloFil[j]);
-                                    if(countG == 3){
-                                        count++;
+                                    if(i == 2 && j == 3){
+                                        validadorRGB = false;
+                                        break;  
                                     }
-                                }else if(count == 2){
-                                    B = B+teclado[i][j]*10^countB;
-                                    countB ++;
-                                    while(!arregloFil[j]);
-                                    if(countB == 3){
-                                        count++;
-                                    }
-                                }else{
-                                    break;
                                 }
                             }
+                            arregloCol[i] =1;
                         }
-                        arregloCol[i] =1;
                     }
-                    frecuenciaR = R/255;
-                    frecuenciaG = G/255;
-                    frecuenciaB = B/255;
+                    cout<<"Valor ingresado para el rojo: "<<R<<endl;
+                    counter = 0;
+                    validadorRGB = true;
+                    cout<<"Ingrese el valor (entre 0 y 255) para el verde"<<endl;
+                    while(validadorRGB){
+                        for(int i =0; i<3; i++){
+                            arregloCol[i] =0;
+                            for(int j = 0; j<4;j++){
+                                if(!arregloFil[j]){
+                                    G += teclado[j][i]*(pow(10,counter));
+                                    counter++;
+                                    while(!arregloFil[j]){
+
+                                    }
+                                    if(i == 2 && j == 3){
+                                        validadorRGB = false;
+                                        break;  
+                                    }
+                                }
+                            }
+                            arregloCol[i] =1;
+                        }
+                    }
+                    cout<<"Valor ingresado para el verde: "<<G<<endl;
+                    counter = 0;
+                    validadorRGB = true;
+                    cout<<"Ingrese el valor (entre 0 y 255) para el azul"<<endl;
+                    while(validadorRGB){
+                        for(int i =0; i<3; i++){
+                            arregloCol[i] =0;
+                            for(int j = 0; j<4;j++){
+                                if(!arregloFil[j]){
+                                    B += teclado[j][i]*(pow(10,counter));
+                                    counter++;
+                                    while(!arregloFil[j]){
+
+                                    }
+                                    if(i == 2 && j == 3){
+                                        validadorRGB = false;
+                                        break;  
+                                    }
+                                }
+                            }
+                            arregloCol[i] =1;
+                        }
+                    }
+                    cout<<"Valor ingresado para el azul: "<<B<<endl;
+                    validadorRGB = true;
+                    frecuenciaR = 1-R/255;
+                    frecuenciaG = 1-G/255;
+                    frecuenciaB = 1-B/255;
                     ledR.write(frecuenciaR);
                     ledG.write(frecuenciaG);
                     ledB.write(frecuenciaB);
                     ThisThread::sleep_for(BLINKING_RATE);
-                    cout<<"Desea otro color(0 para salir),(1 para continuar)"<<endl;
-                    cin>>validador;
+                    cout<<"Desea otro color (0 para salir, 1 para continuar)"<<endl;
+                    while(validadorRGB){
+                        for(int i =0; i<3; i++){
+                            arregloCol[i] =0;
+                            for(int j = 0; j<4;j++){
+                                if(!arregloFil[j]){
+                                    if(i == 0 && j == 0){
+                                        validador = 1;
+                                        validadorRGB = false;
+                                        break;  
+                                    }else if(i == 1 && j == 3){
+                                        validador = 0;
+                                        validadorRGB = false;
+                                        break;
+                                    }
+                                }
+                            }
+                            arregloCol[i] =1;
+                        }
+                    }
                 }
                 xFuncion = -1;
                 yFuncion = -1;
